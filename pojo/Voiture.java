88a7b9a39ac887a3f277ca5eae48465a898a801e
Mahmoud Zine <mr.zine.mahmoud@gmail.com>
2018-08-29 15:07:25 +0100

    L'ajout de code Variable getter setter constructeur a la classe Voiture

---
+++ pojo/Voiture.java
@@ -0,0 +1,15 @@
+public class Voiture{
+	public String type;
+
+	public Voiture(String type){
+		this.type=type;
+	}
+
+    public String getType(){
+    	return this.type;
+    }
+
+    public void setType(String type){
+    	this.type=type;
+    }
+}
