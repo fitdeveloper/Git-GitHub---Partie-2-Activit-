754b8a6cb7749d5cb242adf4cdd8c086a34816ee
Mahmoud Zine <mr.zine.mahmoud@gmail.com>
2018-08-29 15:02:07 +0100

    creation des variables de la class Personne avec un Constructeur

---
+++ pojo/Personne.java
@@ -0,0 +1,10 @@
+public class Personne{
+	private String nom,prenom;
+	private int age;
+	public Personne(String nom,String prenom,int age){
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+
+}
